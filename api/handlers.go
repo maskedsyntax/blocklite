2efe37ae5e86e79d531f8ac244e8d8053ba298ea
maskedsyntax <aftaab2507@gmail.com>
2025-07-10 23:23:26 +0530

    feat: add other apis
    Out of all the apis, only two apis are important:
    - /api/mine
    - /api/full-chain
    
    the rest are there for learning purpose

--- api/handlers.go
+++ api/handlers.go
@@ -3,6 +3,7 @@ package api
 import (
 	"blocklite/blockchain"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,34 +15,82 @@ func GetBlocks(c *gin.Context, bc *blockchain.Blockchain) {
 
 // Add a new block to the blockchain
 func CreateBlock(c *gin.Context, bc *blockchain.Blockchain) {
+	var newBlock blockchain.Block
+
+	if err := c.ShouldBindJSON(&newBlock); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	latestBlock := bc.GetLatestBlock()
+	newProof := blockchain.ProofOfWork(latestBlock.Proof)
+	newBlock = bc.CreateBlock(newProof, latestBlock.CalculateHash())
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Block mined successfully", "block": newBlock})
 }
 
 // Mine a new block with proof of work
 func MineBlock(c *gin.Context, bc *blockchain.Blockchain) {
+	latestBlock := bc.GetLatestBlock()
+	newProof := blockchain.ProofOfWork(latestBlock.Proof)
+	newBlock := bc.CreateBlock(newProof, latestBlock.CalculateHash())
+
+	c.JSON(http.StatusOK, gin.H{"message": "Congratulations! You just mined a block", "block": newBlock})
 }
 
 // Calculate the proof of work for the latest block
-func GetProofWork(c *gin.Context, bc *blockchain.Blockchain) {
+func GetProofOfWork(c *gin.Context, bc *blockchain.Blockchain) {
+	latestBlock := bc.GetLatestBlock()
+	proof := blockchain.ProofOfWork(latestBlock.Proof)
+	c.JSON(http.StatusOK, gin.H{"proof": proof})
 }
 
 // Return the hash of the latest block
 func GetPreviousHash(c *gin.Context, bc *blockchain.Blockchain) {
+	latestBlock := bc.GetLatestBlock()
+	c.JSON(http.StatusOK, gin.H{"previousHash": latestBlock.Proof})
 }
 
 // Retrieve a block by its index
 func GetBlockByIndex(c *gin.Context, bc *blockchain.Blockchain) {
+	index := c.Param("index")
+
+	idx, err := strconv.Atoi(index)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid index"})
+		return
+	}
+
+	block, ok := bc.GetBlockByIndex(idx)
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Block Not Found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, block)
 }
 
 // Return the timestamp of the latest block
 func GetTimestamp(c *gin.Context, bc *blockchain.Blockchain) {
+	latestBlock := bc.GetLatestBlock()
+	c.JSON(http.StatusOK, gin.H{"timestamp": latestBlock.Timestamp})
 }
 
 // Return the number of blocks in the blockchain
 func GetLength(c *gin.Context, bc *blockchain.Blockchain) {
-	c.JSON(http.StatusOK, bc.GetLength())
+	latestBlock := bc.GetLatestBlock()
+	c.JSON(http.StatusOK, gin.H{"length": latestBlock.Timestamp})
 }
 
 // Return the entire blockchain
 func GetFullChain(c *gin.Context, bc *blockchain.Blockchain) {
-	c.JSON(http.StatusOK, bc.Chain)
+	response := struct {
+		Length int                `json:"length"`
+		Chain  []blockchain.Block `json:"chain"`
+	}{
+		Length: bc.GetLength(),
+		Chain:  bc.Chain,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
--- api/routes.go
+++ api/routes.go
@@ -11,7 +11,7 @@ func SetupRoutes(router *gin.Engine, bc *blockchain.Blockchain) {
 	router.GET("/api/blocks", func(c *gin.Context) { GetBlocks(c, bc) })
 	router.POST("/api/blocks", func(c *gin.Context) { CreateBlock(c, bc) })
 	router.POST("/api/mine", func(c *gin.Context) { MineBlock(c, bc) })
-	router.GET("/api/proof", func(c *gin.Context) { GetProofWork(c, bc) })
+	router.GET("/api/proof", func(c *gin.Context) { GetProofOfWork(c, bc) })
 	router.GET("/api/previous-hash", func(c *gin.Context) { GetPreviousHash(c, bc) })
 	router.GET("/api/blocks/:index", func(c *gin.Context) { GetBlockByIndex(c, bc) })
 	router.GET("/api/timestamp", func(c *gin.Context) { GetTimestamp(c, bc) })
